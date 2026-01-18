d1bbb98ed9b487432c2a086815a7fc5c3887ea74
thecynicalcanuck <gabe.devries@gmail.com>
2026-01-18 10:38:29 -0800

    Refactor Mersenne Twister and integrate plugin API
    Refactor Mersenne Twister implementation and integrate with Discourse plugin API.

--- javascripts/discourse/lib/mersenne-twister.js
+++ javascripts/discourse/lib/mersenne-twister.js
@@ -1,224 +1,178 @@
-/* eslint-disable */
-// SPDX-License-Identifier: BSD-3-Clause
+import { getRegister } from "discourse/lib/get-owner";
+import { withPluginApi } from "discourse/lib/plugin-api";
+import WidgetGlue from "discourse/widgets/glue";
+import MersenneTwister from "../lib/mersenne-twister";
+import murmurhash3 from "../lib/murmurhash3";
 
-// This source file consists of three sections under the following licenses:
-// Mersenne Twister implementation, BSD-3-Clause
-// Golang Int31n() function, BSD-3-Clause
-// Discourse-specific glue and modifications, MIT
+const MURMUR_HASH_SEED = 843031067;
 
-/* 
-   A C-program for MT19937, with initialization improved 2002/1/26.
-   Coded by Takuji Nishimura and Makoto Matsumoto.
- 
-   Before using, initialize the state by using init_genrand(seed)  
-   or init_by_array(init_key, key_length).
- 
-   Copyright (C) 1997 - 2002, Makoto Matsumoto and Takuji Nishimura,
-   All rights reserved.                          
- 
-   Redistribution and use in source and binary forms, with or without
-   modification, are permitted provided that the following conditions
-   are met:
- 
-     1. Redistributions of source code must retain the above copyright
-        notice, this list of conditions and the following disclaimer.
- 
-     2. Redistributions in binary form must reproduce the above copyright
-        notice, this list of conditions and the following disclaimer in the
-        documentation and/or other materials provided with the distribution.
- 
-     3. The names of its contributors may not be used to endorse or promote 
-        products derived from this software without specific prior written 
-        permission.
- 
-   THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
-   "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
-   LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
-   A PARTICULAR PURPOSE ARE DISCLAIMED.  IN NO EVENT SHALL THE COPYRIGHT OWNER OR
-   CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
-   EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO,
-   PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR
-   PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF
-   LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
-   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
-   SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- 
- 
-   Any feedback is very welcome.
-   http://www.math.sci.hiroshima-u.ac.jp/~m-mat/MT/emt.html
-   email: m-mat @ math.sci.hiroshima-u.ac.jp (remove space)
-*/
+// [NdN] - base format, \1 is quantity \2 is faces
+// +N or -N - flat modifier, \3 is sign \4 is modifier
+// tN - report pass/fail, \5 is threshold
+// i - report individual dice, \6 is i
+// TODO - kN - keep top N results, \7 is keep count
+const diceRegexp = /(\d+)?d(\d+)?(?:([+-])(\d+))?(?:t(\d+))?(i)?/,
+  idxQuantity = 1,
+  idxFaces = 2,
+  idxModSgn = 3,
+  idxModValue = 4,
+  idxThreshold = 5,
+  idxIndividual = 6;
 
-const MersenneTwister = function(seed) {
-  if (seed == undefined) {
-    seed = new Date().getTime();
-  } 
-  /* Period parameters */  
-  this.N = 624;
-  this.M = 397;
-  this.MATRIX_A = 0x9908b0df;   /* constant vector a */
-  this.UPPER_MASK = 0x80000000; /* most significant w-r bits */
-  this.LOWER_MASK = 0x7fffffff; /* least significant r bits */
- 
-  this.mt = new Array(this.N); /* the array for the state vector */
-  this.mti=this.N+1; /* mti==N+1 means mt[N] is not initialized */
-
-  this.init_genrand(seed);
-}  
- 
-/* initializes mt[N] with a seed */
-MersenneTwister.prototype.init_genrand = function(s) {
-  this.mt[0] = s >>> 0;
-  for (this.mti=1; this.mti<this.N; this.mti++) {
-      var s = this.mt[this.mti-1] ^ (this.mt[this.mti-1] >>> 30);
-   this.mt[this.mti] = (((((s & 0xffff0000) >>> 16) * 1812433253) << 16) + (s & 0x0000ffff) * 1812433253)
-  + this.mti;
-      /* See Knuth TAOCP Vol2. 3rd Ed. P.106 for multiplier. */
-      /* In the previous versions, MSBs of the seed affect   */
-      /* only MSBs of the array mt[].                        */
-      /* 2002/01/09 modified by Makoto Matsumoto             */
-      this.mt[this.mti] >>>= 0;
-      /* for >32 bit machines */
+// returns Object{ quantity, faces, modValue, threshold, individual: Boolean, errors: []i18nkey }
+function parseDice(match) {
+  const errors = [];
+  if (!match) {
+    errors.push("dice.invalid.generic");
+    return { errors };
   }
-}
- 
-/* initialize by an array with array-length */
-/* init_key is the array for initializing keys */
-/* key_length is its length */
-/* slight change for C++, 2004/2/26 */
-MersenneTwister.prototype.init_by_array = function(init_key, key_length) {
-  var i, j, k;
-  this.init_genrand(19650218);
-  i=1; j=0;
-  k = (this.N>key_length ? this.N : key_length);
-  for (; k; k--) {
-    var s = this.mt[i-1] ^ (this.mt[i-1] >>> 30)
-    this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1664525) << 16) + ((s & 0x0000ffff) * 1664525)))
-      + init_key[j] + j; /* non linear */
-    this.mt[i] >>>= 0; /* for WORDSIZE > 32 machines */
-    i++; j++;
-    if (i>=this.N) { this.mt[0] = this.mt[this.N-1]; i=1; }
-    if (j>=key_length) j=0;
-  }
-  for (k=this.N-1; k; k--) {
-    var s = this.mt[i-1] ^ (this.mt[i-1] >>> 30);
-    this.mt[i] = (this.mt[i] ^ (((((s & 0xffff0000) >>> 16) * 1566083941) << 16) + (s & 0x0000ffff) * 1566083941))
-      - i; /* non linear */
-    this.mt[i] >>>= 0; /* for WORDSIZE > 32 machines */
-    i++;
-    if (i>=this.N) { this.mt[0] = this.mt[this.N-1]; i=1; }
-  }
-
-  this.mt[0] = 0x80000000; /* MSB is 1; assuring non-zero initial array */ 
-}
- 
-/* generates a random number on [0,0xffffffff]-interval */
-MersenneTwister.prototype.genrand_int32 = function() {
-  var y;
-  var mag01 = new Array(0x0, this.MATRIX_A);
-  /* mag01[x] = x * MATRIX_A  for x=0,1 */
-
-  if (this.mti >= this.N) { /* generate N words at one time */
-    var kk;
-
-    if (this.mti == this.N+1)   /* if init_genrand() has not been called, */
-      this.init_genrand(5489); /* a default initial seed is used */
-
-    for (kk=0;kk<this.N-this.M;kk++) {
-      y = (this.mt[kk]&this.UPPER_MASK)|(this.mt[kk+1]&this.LOWER_MASK);
-      this.mt[kk] = this.mt[kk+this.M] ^ (y >>> 1) ^ mag01[y & 0x1];
+  const parse = (s) => {
+    if (!s) {
+      return undefined;
     }
-    for (;kk<this.N-1;kk++) {
-      y = (this.mt[kk]&this.UPPER_MASK)|(this.mt[kk+1]&this.LOWER_MASK);
-      this.mt[kk] = this.mt[kk+(this.M-this.N)] ^ (y >>> 1) ^ mag01[y & 0x1];
+    const r = parseInt(s, 10);
+    if (isNaN(r)) {
+      return undefined;
     }
-    y = (this.mt[this.N-1]&this.UPPER_MASK)|(this.mt[0]&this.LOWER_MASK);
-    this.mt[this.N-1] = this.mt[this.M-1] ^ (y >>> 1) ^ mag01[y & 0x1];
-
-    this.mti = 0;
+    return r;
+  };
+  const quantityRaw = parse(match[idxQuantity]),
+    faces = parse(match[idxFaces]),
+    modValueRaw = parse(match[idxModValue]),
+    threshold = parse(match[idxThreshold]);
+  let quantity = quantityRaw;
+  if (quantity === undefined) {
+    quantity = 1;
+  } else if (!(quantity > 0)) {
+    errors.push("dice.invalid.quantity");
+  } else if (quantity > settings.max_dice) {
+    errors.push("dice.excessive.quantity");
   }
-
-  y = this.mt[this.mti++];
-
-  /* Tempering */
-  y ^= (y >>> 11);
-  y ^= (y << 7) & 0x9d2c5680;
-  y ^= (y << 15) & 0xefc60000;
-  y ^= (y >>> 18);
-
-  return y >>> 0;
-}
- 
-/* generates a random number on [0,0x7fffffff]-interval */
-MersenneTwister.prototype.genrand_int31 = function() {
-  return (this.genrand_int32()>>>1);
-}
- 
-/* generates a random number on [0,1]-real-interval */
-MersenneTwister.prototype.genrand_real1 = function() {
-  return this.genrand_int32()*(1.0/4294967295.0); 
-  /* divided by 2^32-1 */ 
-}
-
-/* generates a random number on [0,1)-real-interval */
-MersenneTwister.prototype.random = function() {
-  return this.genrand_int32()*(1.0/4294967296.0); 
-  /* divided by 2^32 */
-}
- 
-/* generates a random number on (0,1)-real-interval */
-MersenneTwister.prototype.genrand_real3 = function() {
-  return (this.genrand_int32() + 0.5)*(1.0/4294967296.0); 
-  /* divided by 2^32 */
-}
- 
-/* generates a random number on [0,1) with 53-bit resolution*/
-MersenneTwister.prototype.genrand_res53 = function() { 
-  var a=this.genrand_int32()>>>5, b=this.genrand_int32()>>>6; 
-  return(a*67108864.0+b)*(1.0/9007199254740992.0); 
-} 
-
-/* These real versions are due to Isaku Wada, 2002/01/09 added */
-
-// Copyright 2020 Discourse.
-// Copyright 2009 The Go Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style
-// license that can be found in the LICENSE file.
-
-// Int26n returns, as an int32, a non-negative pseudo-random number in [0,n).
-// It throws if n <= 0 or n > genrand_int26n_max (over 2^26-1).
-//
-// See https://lemire.me/blog/2016/06/30/fast-random-shuffling/ ,
-// https://lemire.me/blog/2016/06/27/a-fast-alternative-to-the-modulo-reduction/
-MersenneTwister.prototype.genrand_int26n = function(max_) {
-  const max = max_ | 0;
-  if (max <= 0) {
-    throw "invalid argument to genrand_int26n";
+  if (match[idxFaces] === undefined) {
+    errors.push("dice.missing.faces");
+  } else if (!(faces > 1)) {
+    errors.push("dice.invalid.faces");
+  } else if (faces > MersenneTwister.prototype.genrand_int26n_max) {
+    errors.push("dice.excessive.faces");
   }
-  const POW2_27 = 134217728;
-  const MASK2_27 = 134217727;
-  const POW2_26 = 67108864;
-  const MASK2_26 = 67108863;
-  if (max >= POW2_26) {
-    throw "argument to genrand_int26n too large";
+  let modValue = modValueRaw;
+  if (match[idxModValue] !== undefined) {
+    if (!(modValueRaw > 0)) {
+      errors.push("dice.invalid.modifier");
+    }
+    if (match[idxModSgn] === "-") {
+      modValue = -modValueRaw;
+    }
+  } else {
+    modValue = null;
   }
-
-  const val = this.genrand_int32() & MASK2_27;
-  let product = val * max; /* 26 + 27 = up to 2^53 - this is our precision limit */
-  let leftover = product & MASK2_27;
-  if (leftover < max) {
-    // threshold = uint32(-n) % uint32(n)
-    // Manually perform the two's complement (~n+1) and sign bit reinterpretation (+2^n) in the lower base
-    const threshold = ((~max) & MASK2_26 + 1 + POW2_26) % max;
-    while (leftover < threshold) {
-      const reroll = this.genrand_int32() & MASK2_27;
-      product = reroll * max;
-      leftover = product & MASK2_27;
+  if (match[idxThreshold] !== undefined) {
+    if (!(threshold > 0)) {
+      errors.push("dice.invalid.threshold");
     }
   }
-  return (product / POW2_27) | 0;
+  const individual = match[idxIndividual] === "i";
+
+  return {
+    errors,
+    quantity,
+    faces,
+    modValue,
+    threshold,
+    individual,
+    rawInput: null,
+    rawResults: null,
+    crits: null,
+  };
 }
 
-MersenneTwister.prototype.genrand_int26n_max = 67108863;
+// Parses the crit=n,n,n option and writes to attrs.
+function parseCrits(critString, attrs) {
+  const list = critString.split(",").map((s) => parseInt(s, 10));
+  if (list.some(isNaN)) {
+    attrs.errors.push("dice.invalid.crits");
+    return attrs;
+  }
+  attrs.crits = list;
+  return attrs;
+}
 
-export default MersenneTwister;
+function rollDice(rand, attrs) {
+  if (attrs.errors && attrs.errors.length) {
+    return;
+  }
+
+  const results = [];
+  for (let i = 0; i < attrs.quantity; i++) {
+    results.push(1 + rand.genrand_int26n(attrs.faces));
+  }
+
+  attrs.rawResults = results;
+  return attrs;
+}
+
+export default {
+  name: "discourse-dice",
+
+  initialize() {
+    withPluginApi("0.8.7", (api) => {
+      let _glued = [];
+
+      function cleanUp() {
+        _glued.forEach((g) => g.cleanUp());
+        _glued = [];
+      }
+
+      const register = getRegister(api);
+      function attachDiceWidget(container, attrs) {
+        const glue = new WidgetGlue("dice-result", register, attrs);
+        glue.appendTo(container);
+        _glued.push(glue);
+      }
+
+      api.decorateCooked(
+        ($cooked, helper) => {
+          const post = helper?.getModel();
+          let rand = null;
+          if (post) {
+            // Deterministic seed generation: post ID + creation timestamp (UTC ISO8601)
+            const seedData = post.id + " " + post.created_at;
+
+            const seed = murmurhash3(seedData, MURMUR_HASH_SEED);
+            rand = new MersenneTwister(seed);
+          }
+
+          const placeholderNodes = $cooked[0].querySelectorAll(
+            ".d-wrap[data-wrap=dice]"
+          );
+          let seenErrors = false;
+          placeholderNodes.forEach((elem) => {
+            const match = diceRegexp.exec(elem.innerText);
+            const attrs = parseDice(match);
+            attrs.rawInput = (match && match[0]) || elem.innerText;
+            if (elem.dataset.crit) {
+              parseCrits(elem.dataset.crit, attrs);
+            }
+            if (rand && seenErrors) {
+              if (!(attrs.errors && attrs.errors.length > 0)) {
+                attrs.errors = ["dice.invalid.halt_after_error"];
+              }
+            } else if (attrs.errors && attrs.errors.length > 0) {
+              seenErrors = true;
+            } else if (rand) {
+              rollDice(rand, attrs); // attrs.rawResults
+            }
+
+            elem.innerHTML = "";
+            const sacrificial = document.createElement("div");
+            elem.appendChild(sacrificial);
+            attachDiceWidget(sacrificial, attrs);
+          });
+        },
+        { id: "discourse-dice" }
+      );
+
+      api.cleanupStream(cleanUp);
+    });
+  },
+};
